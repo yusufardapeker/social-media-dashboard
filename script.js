6c7551a5e3f740cd8520827dc8d6908f63bb060e
Yusuf Arda Peker <yusufardapkr@gmail.com>
2024-05-10 12:00:07 +0300

    (mobile): light/dark mode done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("naber");
+const toggleButton = document.querySelector(".toggle-btn");
+const toggleCircle = document.querySelector(".toggle-circle");
+
+const bodyElement = document.querySelector("body");
+const headerElement = document.querySelector("header");
+const followersSection = document.querySelector("#followers");
+const overviewSection = document.querySelector("#overview");
+
+toggleButton.addEventListener("click", () => {
+	toggleCircle.classList.toggle("light");
+
+	bodyElement.classList.toggle("light");
+	headerElement.classList.toggle("light");
+	followersSection.classList.toggle("light");
+	overviewSection.classList.toggle("light");
+});
