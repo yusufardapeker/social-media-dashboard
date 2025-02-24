f8814da02842c3cf61ecff3d04bd55f67f211985
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-02-24 23:44:09 +0300

    Fix scope issue

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const toggleButton = document.querySelector(".toggle-btn");
 const body = document.body;
 
 body.dataset.theme = localStorage.getItem("theme");
-const toSetTheme = body.dataset.theme === "light" ? "dark" : "light";
 
 const handleToggle = () => {
+	const toSetTheme = body.dataset.theme === "light" ? "dark" : "light";
 	body.dataset.theme = toSetTheme;
 	localStorage.setItem("theme", toSetTheme);
 };
